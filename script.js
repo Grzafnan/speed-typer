4ad19eddf039b33fadb6513cfcc29d49d0d39ae1
Grzafnan <Grzafnan>
2022-09-15 19:29:55 +0600

    stop space scorll

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
 
+
+// Stop space scroll 
+window.onkeydown = function (e) {
+  return !(e.keyCode == 32);
+};
+
 // variables
 let userText = "";
 let errorCount = [];
